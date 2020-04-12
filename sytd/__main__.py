bb819f5d037590930b874ad74ce46fe43ce22f58
tomg404 <32769321+tomg404@users.noreply.github.com>
2020-04-12 04:51:13 +0200

    dev

--- sytd/__main__.py
+++ sytd/__main__.py
@@ -5,6 +5,7 @@ from tkinter import Tk, filedialog
 import youtube_dl as yt
 import configparser
 from threading import Thread
+from __init__ import __version__ as version
 
 # setup eels root folder and config location
 web_location = 'web'
@@ -14,11 +15,13 @@ eel.init(web_path)
 config_location = 'config.ini'
 config_path = os.path.dirname(os.path.realpath(__file__)) + '/' + config_location
 
+# starts the download function in a thread
 @eel.expose
 def download(url):
     t = Thread(target=download_thread, args=(url,))
     t.start()
 
+# actual downloading function
 def download_thread(url):
     try:
         ydl_opts = {'verbose': 'false',
@@ -37,7 +40,6 @@ def download_thread(url):
         print('An error occured! Please check your URL. ❌')
         eel.update_status('An error occured! Please check your URL. ❌')
 
-
 # updates the progress bar as the download goes on
 def my_hook(d):
     total_bytes = int(d['total_bytes'])
@@ -45,12 +47,13 @@ def my_hook(d):
     percentage = round((downloaded_bytes / total_bytes) * 100)
     eel.update_progressbar(percentage)
 
+# opens a explorer window to select output directory
 @eel.expose
 def open_dir_browser():
     root = Tk()
     root.withdraw()
     root.wm_attributes('-topmost', 1)
-    folder = filedialog.askdirectory().replace('\\', '/')  # open directory selector
+    folder = filedialog.askdirectory()  # open directory selector
 
     config = configparser.ConfigParser()    # write selected directory to config
     config.read(config_path)
@@ -58,12 +61,6 @@ def open_dir_browser():
     with open(config_path, 'w') as configfile:
         config.write(configfile)
     update_status_output()
-
-# return version number from config.ini
-def get_version():
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    return config['MAIN']['version']
 
 # return the current output path
 def get_save_path():
@@ -81,15 +78,12 @@ def check_config():
     if not os.path.isfile(config_path):
         print('missing config.ini, creating new')
         with open(config_path, 'a') as f:
-            f.write('[MAIN]\n')
-            f.write('version = 1.0.0\n')
-            f.write('save_path = \n')
+            f.write('[MAIN]\nsave_path = \n')
 
 def run():
     check_config()
     update_status_output()
-    VERSION = get_version()
-    eel.update_version_badge('v' + VERSION)
+    eel.update_version_badge('v' + version)
     try:
         eel.start('main.html', mode='chrome', port=0, size=(600, 800))
     except (SystemExit, KeyboardInterrupt):
